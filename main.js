85211a2cda50187d0743d402b528a64adf9db79e
malred <99012268+malred@users.noreply.github.com>
2023-01-06 11:44:50 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-const { app, BrowserWindow } = require('electron') 
+const { app, BrowserWindow } = require('electron')
 const path = require('path')
+const createTray = require('./mainTray')
 const WinState = require('electron-win-state').default
 const winState = new WinState({
     defaultWidth: 800,
@@ -16,10 +17,15 @@ const createWindow = () => {
     })
     win.on('ready-to-show', win.show)
     // 本地起vue3项目
-    win.loadURL('http://localhost:3000')    
+    // win.loadURL('http://localhost:3000')  
+    process.env.DIST = path.join(__dirname, './')
+    const indexHtml = path.join(process.env.DIST, 'dist/index.html')
+    win.loadFile(indexHtml)
     // 打开调试器
     win.webContents.openDevTools()
     winState.manage(win)
+    // 创建托盘,托盘的关闭可以关掉后台 
+    createTray(app, win)
 }
 app.whenReady().then(createWindow)
 app.on('window-all-closed', () => {
